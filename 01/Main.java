b00158151be178d12ad318274856e34e2bac91da
Myster7494 <jackychiu0101@gmail.com>
2024-06-30 10:34:22 +0800

    01

---
+++ 01/Main.java
@@ -0,0 +1,11 @@
+// class 名稱必須跟檔案名稱一樣
+public class Main {
+
+    // 任何一個 Java 程式都需要一個主函式(main 函式)
+    public static void main(String[] args) {
+
+        // 在 Java 中，使用 System.out.println() 來輸出資料
+        System.out.println("Hello, World!");
+
+    }
+}
