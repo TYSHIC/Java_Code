26611618be9a08aa2026b23f4750d56c2b6ef58b
Myster7494 <jackychiu0101@gmail.com>
2024-07-06 22:25:21 +0800

    02

--- 01/Main.java
+++ 01/Main.java
@@ -1,7 +1,7 @@
 // class 名稱必須跟檔案名稱一樣
 public class Main {
 
-    // 任何一個 Java 程式都需要一個主函式(main 函式)
+    // Java 程式需要一個主方法(main 方法)，程式從這裡開始執行
     public static void main(String[] args) {
 
         // 在 Java 中，使用 System.out.println() 來輸出資料
---
+++ 02/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("a");
+        System.out.println('a');
+        System.out.println("2147483647");
+        System.out.println(2147483647);
+        System.out.println("3.14159");
+        System.out.println(3.14159);
+        System.out.println("true");
+        System.out.println(true);
+    }
+}
