5a6ff49dda191e6d0ebadfa45d2703b47ff8f13e
Myster7494 <jackychiu0101@gmail.com>
2024-07-07 02:28:54 +0800

    03

---
+++ 03/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(2);
+        int a = 2;
+        System.out.println(a);
+        a = 0;
+        System.out.println(a);
+        System.out.println(a = 3); // 賦值作為表達式
+        System.out.println(a);
+    }
+}
