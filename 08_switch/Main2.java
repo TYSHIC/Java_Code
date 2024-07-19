760e61fe69400e967cce271109b87c897b72a91f
Myster7494 <jackychiu0101@gmail.com>
2024-07-19 23:59:01 +0800

    08_switch

--- 08_switch/Main2.java
+++ 08_switch/Main2.java
@@ -7,7 +7,7 @@ public class Main2 {
         switch (input) {
             case "回憶過去" -> System.out.println("痛苦的相思忘不了");
             case "想念你的笑" -> System.out.println("想念你的外套");
-            case "怎麼忍心怪你犯了錯" -> System.out.println("是我給你自由過了火");
+            case "怎麼忍心怪你犯了錯", "怎麼忍心讓你受折磨" -> System.out.println("是我給你自由過了火");
             case "我願變成童話裡你愛的那個天使" -> System.out.println("張開雙手變成翅膀守護你");
             default -> System.out.println("我不知道下一句是什麼");
         }
--- 08_switch/Main3.java
+++ 08_switch/Main3.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Main3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
+        String input = scanner.nextLine();
         String output = switch (input) {
             case "回憶過去" -> "痛苦的相思忘不了";
             case "想念你的笑" -> "想念你的外套";
-            case "怎麼忍心怪你犯了錯" -> "是我給你自由過了火";
+            case "怎麼忍心怪你犯了錯", "怎麼忍心讓你受折磨" -> "是我給你自由過了火";
             case "我願變成童話裡你愛的那個天使" -> "張開雙手變成翅膀守護你";
-            default -> "我不知道下一句是什麼";
+            default -> {
+                if (input.isEmpty()) yield "沒有輸入任何東西";
+                yield "我不知道下一句是什麼";
+            }
         };
         System.out.println(output);
     }
