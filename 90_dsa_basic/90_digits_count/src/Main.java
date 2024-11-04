d660ae388ed16cad80181f4850c32a7c1e3b9a40
Myster7494 <jackychiu0101@gmail.com>
2024-11-04 23:13:38 +0800

    90_each_digit

---
+++ 90_dsa_basic/90_digits_count/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.printf("%d has %d digit(s).", n, (int) Math.log10(n) + 1);
+    }
+}
---
+++ 90_dsa_basic/90_each_digit/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        while (n != 0) {
+            System.out.println(n % 10);
+            n /= 10;
+        }
+    }
+}
