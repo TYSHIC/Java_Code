b6b5ac6e41a3a0bed2d0e9094c9fa0d51ddb0110
Myster7494 <jackychiu0101@gmail.com>
2024-11-21 23:31:26 +0800

    feat: binary search

---
+++ 90_dsa_basic/src/Main6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // 獲取資料個數
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = scanner.nextInt(); // 讀入資料
+        int target = scanner.nextInt(); // 讀入目標資料
+
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] == target) {
+                System.out.println(mid + 1);
+                return;
+            }
+            if (arr[mid] > target) r = mid - 1;
+            else l = mid + 1;
+        }
+        System.out.println("Nearest Position: " + (l + 1));
+    }
+}
